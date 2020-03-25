470fbdab3b4cbcb09934829f8724bf273c2e96d0
pannenkoekenmetchoco <48311990+pannenkoekenmetchoco@users.noreply.github.com>
2020-03-25 12:33:17 +0100

    bank en client gemaakt

---
+++ src/main/java/com/bank/Bank.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import java.util.ArrayList;
+
+public class Bank {
+    public static void main(String[] args) {
+        //SpringApplication.run(App.class, args);
+        ArrayList<Client> accounts = new ArrayList<Client>();
+
+        public int
+
+    }
+}
---
+++ src/main/java/com/bank/Client.java
@@ -0,0 +1,10 @@
+package com.bank;
+
+public class Client {
+    public static void main(String[] args){
+        int balance;
+        int addMoney;
+        int getMoney;
+
+    }
+}
