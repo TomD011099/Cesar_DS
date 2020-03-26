77edd8c5cbd88143ed778ee51431c4656cc0ffaa
Tom De bruyn <tom.de.bruyn99@gmail.com>
2020-03-26 10:30:55 +0100

    Clean up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException {
