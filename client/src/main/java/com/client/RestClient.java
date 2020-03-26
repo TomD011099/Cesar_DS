b964aa8b2becd371ecc642d4c12f53e04a1355d8
Milan Cornelis <milan.cornelis@hotmail.com>
2020-03-26 09:40:23 +0100

    Cleaned pom | deleted test file

--- client/src/main/java/com/client/RestClient.java
+++ client/src/main/java/com/client/RestClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
  */
 public class RestClient {
 
-    private String server = "http://143.129.39.106:30013"; // IP from server
+    private String server = "http://143.129.39.106:30013";
     private RestTemplate rest;
     private HttpHeaders headers;
     private HttpStatus status;
