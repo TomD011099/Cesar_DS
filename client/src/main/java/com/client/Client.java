44916b9bb5fd788fea56b5417d5599b232443aef
Milan Cornelis <milan.cornelis@hotmail.com>
2020-03-26 09:01:48 +0100

    removed get client

--- client/src/main/java/com/client/Client.java
+++ client/src/main/java/com/client/Client.java
@@ -9,6 +9,5 @@ public class Client {
         RestClient client = new RestClient();
         System.out.println(client.post("create?name=Milan", null));
         System.out.println(client.get("balance?id=1")); // get balance
-        System.out.println(client.get("customers"));
     }
 }
