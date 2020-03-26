3407960230fd7526d671394a6ad2cd560e80ab23
Milan Cornelis <milan.cornelis@hotmail.com>
2020-03-26 08:51:22 +0100

    PUT

--- client/src/main/java/com/client/Client.java
+++ client/src/main/java/com/client/Client.java
@@ -8,6 +8,6 @@ public class Client {
     public static void main(String[] args) {
         RestClient client = new RestClient();
         System.out.println(client.post("create?name=Milan", null));
-        System.out.println(client.get("balance?id=1"));
+        System.out.println(client.get("balance?id=1")); // get balance
     }
 }
