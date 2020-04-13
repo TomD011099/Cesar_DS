5d8bdae260b9570f8d968b832df140e526c05ff3
Razarik <48281364+Razarik@users.noreply.github.com>
2020-04-13 10:41:09 +0200

    multicast finished (I think)

--- src/main/java/MulticastReceiver.java
+++ src/main/java/MulticastReceiver.java
@@ -2,28 +2,60 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
 
-public class MulticastReceiver extends Thread {
+public class MulticastReceiver implements Runnable {
     protected MulticastSocket socket = null;
-    protected byte[] buf = new byte[256];
+    private volatile CesarString nodeName = null;
+    private volatile InetAddress clientAddr = null;
+    private boolean running = false;
 
-    public void run(){
+    public void run() {
         try {
             socket = new MulticastSocket(4446);
             InetAddress group = InetAddress.getByName("230.0.0.0");
             socket.joinGroup(group);
+            socket.setSoTimeout(10000);
             while (true) {
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);
-                String received = new String(packet.getData(), 0, packet.getLength());
-                if ("end".equals(received)) {
+                try {
+                    running = true;
+                    // Read the path to the file that has to be copied
+                    byte[] receive = new byte[65535];
+                    DatagramPacket nodeInfoPacket = new DatagramPacket(receive, receive.length);
+                    socket.receive(nodeInfoPacket);
+                    String strNodeName = new String(nodeInfoPacket.getData());
+                    if ("end".equals(strNodeName)) {
+                        break;
+                    }
+                    nodeName = new CesarString(strNodeName);
+                    System.out.println("Node" + nodeName + " detected.");
+                    clientAddr = nodeInfoPacket.getAddress();
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Timeout reached");
+                    running = false;
+                    socket.leaveGroup(group);
+                    socket.close();
                     break;
                 }
             }
+            running = false;
             socket.leaveGroup(group);
             socket.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
+            running = false;
         }
+    }
+
+    public CesarString getNodeName() {
+        return nodeName;
+    }
+
+    public InetAddress getInetAddress() {
+        return clientAddr;
+    }
+
+    public boolean isRunning() {
+        return running;
     }
 }
