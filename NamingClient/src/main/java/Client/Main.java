8cf37be03ab3e6ec2a4c699b4715894b87d4f60d
Milan Cornelis <milan.cornelis@hotmail.com>
2020-05-06 16:45:37 +0200

    ?

--- NamingClient/src/main/java/Client/Main.java
+++ NamingClient/src/main/java/Client/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*if (args.length != 2) {
+        if (args.length != 2) {
             System.out.println("Not the right amount of args");
             System.out.println("Should be: <name> <ip-address>");
             return;
-        }*/
+        }
 
-        String name = "127.0.0.1"; //args[0];
-        String ip = "127.0.0.1"; //args[1];
+        String name = args[0];
+        String ip = args[1];
         Client client;
         try {
-            client = new Client("./local"/*"/home/pi/local/"*/,"./remote" /*"/home/pi/remote/"*/, "./"/*"/home/pi/request/"*/, name, ip);
+            client = new Client("/home/pi/local/","/home/pi/remote/", "/home/pi/request/", name, ip);
             client.run();
         } catch (NodeNotRegisteredException | UnknownHostException e) {
             System.err.println(e.getMessage());
